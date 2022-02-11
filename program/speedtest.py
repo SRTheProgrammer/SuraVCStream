f5dff6e5c2c2e511f941d3aa493da7fe9a207197
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-11 19:17:40 +0530

    Update speedtest.py

--- program/speedtest.py
+++ program/speedtest.py
@@ -8,7 +8,7 @@ from program.utils.formatters import bytes
 from driver.filters import command, other_filters
 from driver.decorators import sudo_users_only
 from config import BOT_USERNAME as bname
-from driver.surabot import bot as app
+from driver.core import bot as app
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
@@ -43,7 +43,6 @@ async def run_speedtest(_, message: Message):
 **Country:** {result['server']['country']}, {result['server']['cc']}
 **Sponsor:** {result['server']['sponsor']}
 **Latency:** {result['server']['latency']}
-
 ⚡️ **Ping:** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=path, caption=output
