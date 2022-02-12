0c212737f916d7849c1a39be676d5f0d259273cc
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-12 18:59:19 +0530

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -1,176 +1,106 @@
-""" global banned and un-global banned module """
-
-
 import asyncio
 
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.errors import FloodWait
+from config import BOT_USERNAME, SUDO_USERS
+
+from driver.core import user
 from driver.filters import command, other_filters
-from driver.decorators import bot_creator
-from driver.database.dbchat import get_served_chats
-from driver.database.dbpunish import add_gban_user, is_gbanned_user, remove_gban_user
+from driver.database.dbchat import remove_served_chat
+from driver.database.dbqueue import remove_active_chat
+from driver.database.dbpunish import is_gbanned_user
+from driver.decorators import authorized_users_only, bot_creator
 
-from config import SUDO_USERS, BOT_USERNAME as bn
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 
 
-@Client.on_message(command(["gban", f"gban@{bn}"]) & other_filters)
-@bot_creator
-async def global_banned(c: Client, message: Message):
-    BOT_NAME = (await c.get_me()).first_name
-    if not message.reply_to_message:
-        if len(message.command) < 2:
-            await message.reply_text("**usage:**\n\n/gban [username | user_id]")
-            return
-        user = message.text.split(None, 2)[1]
-        if "@" in user:
-            user = user.replace("@", "")
-        user = await c.get_users(user)
-        from_user = message.from_user
-        BOT_ID = await c.get_me()
-        if user.id == from_user.id:
-            return await message.reply_text(
-                "You can't gban yourself !"
-            )
-        elif user.id == BOT_ID:
-            await message.reply_text("I can't gban myself !")
-        elif user.id in SUDO_USERS:
-            await message.reply_text("You can't gban sudo user !")
-        else:
-            await add_gban_user(user.id)
-            served_chats = []
-            chats = await get_served_chats()
-            for chat in chats:
-                served_chats.append(int(chat["chat_id"]))
-            m = await message.reply_text(
-                f"🚷 **Globally banning {user.mention}**\n⏱ Expected time: `{len(served_chats)}`"
-            )
-            number_of_chats = 0
-            for num in served_chats:
-                try:
-                    await c.ban_chat_member(num, user.id)
-                    number_of_chats += 1
-                    await asyncio.sleep(1)
-                except FloodWait as e:
-                    await asyncio.sleep(int(e.x))
-                except Exception:
-                    pass
-            ban_text = f"""
-🚷 **New Global ban on [{BOT_NAME}](https://t.me/{bn})
-**Origin:** {message.chat.title} [`{message.chat.id}`]
-**Sudo User:** {from_user.mention}
-**Banned User:** {user.mention}
-**Banned User ID:** `{user.id}`
-**Chats:** `{number_of_chats}`"""
-            try:
-                await m.delete()
-            except Exception:
-                pass
-            await message.reply_text(
-                f"{ban_text}",
-                disable_web_page_preview=True,
-            )
+
+@Client.on_message(
+    command(["userbotjoin", f"userbotjoin@{BOT_USERNAME}"]) & other_filters
+)
+@authorized_users_only
+async def join_chat(c: Client, m: Message):
+    chat_id = m.chat.id
+    user_id = m.from_user.id
+    if await is_gbanned_user(user_id):
+        await m.reply_text("❗️ **You've blocked from using this bot!**")
         return
-    from_user_id = message.from_user.id
-    from_user_mention = message.from_user.mention
-    user_id = message.reply_to_message.from_user.id
-    mention = message.reply_to_message.from_user.mention
-    BOT_ID = await c.get_me()
-    if user_id == from_user_id:
-        await message.reply_text("You can't gban yourself !")
-    elif user_id == BOT_ID:
-        await message.reply_text("I can't gban myself !")
-    elif user_id in SUDO_USERS:
-        await message.reply_text("You can't gban sudo user !")
-    else:
-        is_gbanned = await is_gbanned_user(user_id)
-        if is_gbanned:
-            await message.reply_text("This user already gbanned !")
-        else:
-            await add_gban_user(user_id)
-            served_chats = []
-            chats = await get_served_chats()
-            for chat in chats:
-                served_chats.append(int(chat["chat_id"]))
-            m = await message.reply_text(
-                f"🚷 **Globally banning {mention}**\n⏱ Expected time: `{len(served_chats)}`"
+    try:
+        invitelink = (await c.get_chat(chat_id)).invite_link
+        if not invitelink:
+            await c.export_chat_invite_link(chat_id)
+            invitelink = (await c.get_chat(chat_id)).invite_link
+        if invitelink.startswith("https://t.me/+"):
+            invitelink = invitelink.replace(
+                "https://t.me/+", "https://t.me/joinchat/"
             )
-            number_of_chats = 0
-            for num in served_chats:
-                try:
-                    await c.ban_chat_member(num, user_id)
-                    number_of_chats += 1
-                    await asyncio.sleep(1)
-                except FloodWait as e:
-                    await asyncio.sleep(int(e.x))
-                except Exception:
-                    pass
-            ban_text = f"""
-🚷 **New Global ban on [{BOT_NAME}](https://t.me/{bn})
-**Origin:** {message.chat.title} [`{message.chat.id}`]
-**Sudo User:** {from_user_mention}
-**Banned User:** {mention}
-**Banned User ID:** `{user_id}`
-**Chats:** `{number_of_chats}`"""
-            try:
-                await m.delete()
-            except Exception:
-                pass
-            await message.reply_text(
-                f"{ban_text}",
-                disable_web_page_preview=True,
-            )
-            return
+        await user.join_chat(invitelink)
+        await remove_active_chat(chat_id)
+        return await user.send_message(chat_id, "✅ userbot joined chat")
+    except UserAlreadyParticipant:
+        return await user.send_message(chat_id, "✅ userbot already in chat")
 
 
-@Client.on_message(command(["ungban", f"ungban@{bn}"]) & other_filters)
-@bot_creator
-async def ungban_global(c: Client, message: Message):
-    chat_id = message.chat.id
-    if not message.reply_to_message:
-        if len(message.command) != 2:
-            await message.reply_text(
-                "**usage:**\n\n/ungban [username | user_id]"
-            )
-            return
-        user = message.text.split(None, 1)[1]
-        if "@" in user:
-            user = user.replace("@", "")
-        user = await c.get_users(user)
-        from_user = message.from_user
-        BOT_ID = await c.get_me()
-        if user.id == from_user.id:
-            await message.reply_text("You can't ungban yourself because you can't be gbanned !")
-        elif user.id == BOT_ID:
-            await message.reply_text("I can't ungban myself because i can't be gbanned !")
-        elif user.id in SUDO_USERS:
-            await message.reply_text("Sudo users can't be gbanned/ungbanned !")
-        else:
-            is_gbanned = await is_gbanned_user(user.id)
-            if not is_gbanned:
-                await message.reply_text("This user not ungbanned !")
-            else:
-                await c.unban_chat_member(chat_id, user.id)
-                await remove_gban_user(user.id)
-                await message.reply_text("✅ This user has ungbanned")
+@Client.on_message(
+    command(["userbotleave", f"userbotleave@{BOT_USERNAME}"]) & other_filters
+)
+@authorized_users_only
+async def leave_chat(_, m: Message):
+    chat_id = m.chat.id
+    user_id = m.from_user.id
+    if await is_gbanned_user(user_id):
+        await m.reply_text("❗️ **You've blocked from using this bot!**")
         return
-    from_user_id = message.from_user.id
-    user_id = message.reply_to_message.from_user.id
-    mention = message.reply_to_message.from_user.mention
-    BOT_ID = await c.get_me()
-    if user_id == from_user_id:
-        await message.reply_text("You can't ungban yourself because you can't be gbanned !")
-    elif user_id == BOT_ID:
-        await message.reply_text(
-            "I can't ungban myself because i can't be gbanned !"
+    try:
+        await user.leave_chat(chat_id)
+        await remove_active_chat(chat_id)
+        return await _.send_message(
+            chat_id,
+            "✅ userbot leaved chat",
         )
-    elif user_id in SUDO_USERS:
-        await message.reply_text("Sudo users can't be gbanned/ungbanned !")
-    else:
-        is_gbanned = await is_gbanned_user(user_id)
-        if not is_gbanned:
-            await message.reply_text("This user not gbanned !")
-        else:
-            await c.unban_chat_member(chat_id, user_id)
-            await remove_gban_user(user_id)
-            await message.reply_text("✅ This user has ungbanned")
+    except UserNotParticipant:
+        return await _.send_message(
+            chat_id,
+            "❌ userbot already leave chat",
+        )
+
+
+@Client.on_message(command(["leaveall", f"leaveall@{BOT_USERNAME}"]))
+@bot_creator
+async def leave_all(client, message):
+    if message.from_user.id not in SUDO_USERS:
+        return
+
+    left = 0
+    failed = 0
+    
+    msg = await message.reply("🔄 Userbot leaving all Group !")
+    async for dialog in user.iter_dialogs():
+        try:
+            await user.leave_chat(dialog.chat.id)
+            await remove_active_chat(dialog.chat.id)
+            left += 1
+            await msg.edit(
+                f"Userbot leaving all Group...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+            )
+        except BaseException:
+            failed += 1
+            await msg.edit(
+                f"Userbot leaving...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+            )
+        await asyncio.sleep(0.7)
+    await msg.delete()
+    await client.send_message(
+        message.chat.id, f"✅ Left from: {left} chats.\n❌ Failed in: {failed} chats."
+    )
+
+
+@Client.on_message(filters.left_chat_member)
+async def bot_kicked(c: Client, m: Message):
+    bot_id = (await c.get_me()).id
+    chat_id = m.chat.id
+    left_member = m.left_chat_member
+    if left_member.id == bot_id:
+        await user.leave_chat(chat_id)
+        await remove_served_chat(chat_id)
+        await remove_active_chat(chat_id)
