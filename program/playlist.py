c11e584513b71e09df2ff89be7f41d6e50c3039b
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-14 20:19:25 +0530

    Update playlist.py

--- program/playlist.py
+++ program/playlist.py
@@ -1,6 +1,4 @@
-# Copyright (C) 2021 By Veez Music-Project
-# Commit Start Date 20/10/2021
-# Finished On 28/10/2021
+#Copyright 2022 SuraVCProject
 
 from config import BOT_USERNAME
 from pyrogram.types import (
@@ -10,30 +8,34 @@ from pyrogram.types import (
     Message,
 )
 from pyrogram import Client, filters
+
+from driver.decorators import check_blacklist
 from driver.queues import QUEUE, get_queue
 from driver.filters import command, other_filters
+from driver.database.dbpunish import is_gbanned_user
 
 
 keyboard = InlineKeyboardMarkup(
-    [[InlineKeyboardButton("🗑 Close", callback_data="cls")]]
+    [[InlineKeyboardButton("🗑 Close", callback_data="set_close")]]
 )
 
 
 @Client.on_message(command(["playlist", f"playlist@{BOT_USERNAME}", "queue", f"queue@{BOT_USERNAME}"]) & other_filters)
+@check_blacklist()
 async def playlist(client, m: Message):
-   chat_id = m.chat.id
-   if chat_id in QUEUE:
-      chat_queue = get_queue(chat_id)
-      if len(chat_queue)==1:
-         await m.reply(f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
-      else:
-         QUE = f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 Queue List:**\n"
-         l = len(chat_queue)
-         for x in range (1, l):
-            han = chat_queue[x][0]
-            hok = chat_queue[x][2]
-            hap = chat_queue[x][3]
-            QUE = QUE + "\n" + f"**#{x}** - [{han}]({hok}) | `{hap}`"
-         await m.reply(QUE, reply_markup=keyboard, disable_web_page_preview=True)
-   else:
-      await m.reply("❌ **nothing is currently streaming.**")
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        chat_queue = get_queue(chat_id)
+        if len(chat_queue)==1:
+            await m.reply(f"💡 **Currently Streaming**`:`\n\n*️⃣ [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
+        else:
+            QUE = f"💡 **Currently Streaming**`:`\n\n*️⃣ [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 Queue song list**`:`\n"
+            l = len(chat_queue)
+            for x in range (1, l):
+                han = chat_queue[x][0]
+                hok = chat_queue[x][2]
+                hap = chat_queue[x][3]
+                QUE = QUE + "\n" + f"`#{x}` - [{han}]({hok}) | `{hap}`"
+            await m.reply(QUE, reply_markup=keyboard, disable_web_page_preview=True)
+    else:
+        await m.reply("❌ **nothing is currently streaming.**")
