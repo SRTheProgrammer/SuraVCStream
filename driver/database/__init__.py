248fee4fca5d3d36f460cb3ff80a9651300ae999
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-19 19:52:03 +0530

    Create __init__.py

---
+++ driver/database/__init__.py
@@ -0,0 +1,4 @@
+""" database copyright """
+
+# thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
+# implemented by SR The Programmer (https://github.com/SRTheProgrammer)
