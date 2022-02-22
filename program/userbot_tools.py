76659d375b9f048f8785a4675e2063dc9b70a45d
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-22 18:20:51 +0530

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -2,6 +2,8 @@ import asyncio
 
 from config import BOT_USERNAME, SUDO_USERS
 
+from program.utils.function import get_calls
+
 from driver.core import user, me_bot
 from driver.filters import command, other_filters
 from driver.database.dbchat import remove_served_chat
@@ -10,8 +12,9 @@ from driver.decorators import authorized_users_only, bot_creator, check_blacklis
 
 from pyrogram.types import Message
 from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
-
+from pyrogram.raw.types import InputPeerChannel
+from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant, ChatAdminRequired
 
 
 @Client.on_message(
@@ -58,7 +61,7 @@ async def leave_chat(_, m: Message):
         )
 
 
-@Client.on_message(command(["leaveall", f"leaveall@{BOT_USERNAME}"]))
+@Client.on_message(command(["leaveall", f"leaveall@{BOT_USERNAME}"]) & ~filters.edited)
 @bot_creator
 async def leave_all(client, message):
     if message.from_user.id not in SUDO_USERS:
@@ -86,6 +89,51 @@ async def leave_all(client, message):
     await client.send_message(
         message.chat.id, f"✅ Left from: {left} chats.\n❌ Failed in: {failed} chats."
     )
+
+
+@Client.on_message(command(["startvc", f"startvc@{BOT_USERNAME}"]) & other_filters)
+@check_blacklist()
+@authorized_users_only
+async def start_group_call(c: Client, m: Message):
+    chat_id = m.chat.id
+    msg = await c.send_message(chat_id, "`starting...`")
+    try:
+        peer = await user.resolve_peer(chat_id)
+        await user.send(
+            CreateGroupCall(
+                peer=InputPeerChannel(
+                    channel_id=peer.channel_id,
+                    access_hash=peer.access_hash,
+                ),
+                random_id=user.rnd_id() // 9000000000,
+            )
+        )
+        await msg.edit_text("✅ Group call started !")
+    except ChatAdminRequired:
+        await msg.edit_text(
+            "The userbot is not admin in this chat. To start the Group call you must promote the userbot as admin first with permission:\n\n» ❌ manage_video_chats"
+        )
+
+
+@Client.on_message(command(["stopvc", f"stopvc@{BOT_USERNAME}"]) & other_filters)
+@check_blacklist()
+@authorized_users_only
+async def stop_group_call(c: Client, m: Message):
+    chat_id = m.chat.id
+    msg = await c.send_message(chat_id, "`stopping...`")
+    if not (
+        group_call := (
+            await get_calls(m, err_msg="group call not active")
+        )
+    ):
+        await msg.edit_text("❌ The group call already ended")
+        return
+    await user.send(
+        DiscardGroupCall(
+            call=group_call
+        )
+    )
+    await msg.edit_text("✅ Group call has ended !")
 
 
 @Client.on_message(filters.left_chat_member)
