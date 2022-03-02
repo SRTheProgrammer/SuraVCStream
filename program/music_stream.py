1c5e43a2268baa38b0caa09b1ff9d5b198042470
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-03-02 22:20:53 +0530

    nothing

--- program/music_stream.py
+++ program/music_stream.py
@@ -365,4 +365,3 @@ async def play(c: Client, m: Message):
                             await m.reply_text("❌ The content you provide to play has no audio source.\n\n» Try to play another song or try again later !")
                         except BaseException as err:
                             print(err)
-
